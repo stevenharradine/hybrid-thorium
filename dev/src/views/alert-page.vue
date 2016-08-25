53a588f8c7cfe7ab9cbd84daafe73cf3398fda74
xingbo828 <xingbo828@gmail.com>
2016-08-25 13:32:58 -0700

    fix doc

--- dev/src/views/alert-page.vue
+++ dev/src/views/alert-page.vue
@@ -43,7 +43,7 @@ export default {
   data() {
     return {
       dismissCallBack() {
-        window.alert('alert dismissed.');
+        console.log('alert!!!');
       },
       apiItems: [{
         name: 'type',
